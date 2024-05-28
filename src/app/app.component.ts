717c1a679413476ef786d583bc8c537181a2d926
shemzz <davidshemang@gmail.com>
2024-05-28 15:11:34 +0100

    setup analytics service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+// biome-ignore lint/style/useImportType: <explanation>
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnalyticsService } from './services';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [AnalyticsService],
 })
-export class AppComponent {
-  title = 'webApp';
+export class AppComponent implements OnInit {
+  constructor(private analytics: AnalyticsService) {}
+
+  ngOnInit() {
+    this.analytics.trackEvent('App Loaded', 'App Loaded', 'App Loaded');
+  }
 }
---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+declare let gtag: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+		trackEvent(eventName: string, eventDetails: string, eventCategory: string) {
+			gtag("event", eventName, {
+				event_category: eventCategory,
+				event_label: eventName,
+				value: eventDetails,
+			});
+		}
+
+	}
