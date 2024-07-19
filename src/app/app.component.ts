5ea3e0f61a8f11bc483af8e31ba198ddcde4a654
shemzz <davidshemang@gmail.com>
2024-07-19 22:34:35 +0100

    remove analytics service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AnalyticsService } from './services';
 import { NavComponent } from "./components/nav/nav.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { IntegrationsComponent } from "./components/integrations/integrations.component";
@@ -13,12 +11,7 @@ import { FooterComponent } from "./components/footer/footer.component";
   imports: [RouterOutlet, NavComponent, HeroComponent, IntegrationsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [AnalyticsService],
 })
-export class AppComponent implements OnInit {
-  constructor(private analytics: AnalyticsService) {}
+export class AppComponent {
 
-  ngOnInit() {
-    this.analytics.trackEvent('App Loaded', 'App Loaded', 'App Loaded');
-  }
 }
