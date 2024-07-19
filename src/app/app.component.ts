7ca7980d16a9002b7130a49b34db8528932277ef
shemzz <davidshemang@gmail.com>
2024-07-19 22:14:23 +0100

    add marketplace integrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { AnalyticsService } from './services';
 import { NavComponent } from "./components/nav/nav.component";
 import { HeroComponent } from "./components/hero/hero.component";
+import { IntegrationsComponent } from "./components/integrations/integrations.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HeroComponent],
+  imports: [RouterOutlet, NavComponent, HeroComponent, IntegrationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [AnalyticsService],
