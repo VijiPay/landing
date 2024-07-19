7b9e12d33936b4fbb13fcac2e5e8b1dc1d3f63cb
shemzz <davidshemang@gmail.com>
2024-07-19 22:19:23 +0100

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AnalyticsService } from './services';
 import { NavComponent } from "./components/nav/nav.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { IntegrationsComponent } from "./components/integrations/integrations.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HeroComponent, IntegrationsComponent],
+  imports: [RouterOutlet, NavComponent, HeroComponent, IntegrationsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [AnalyticsService],
